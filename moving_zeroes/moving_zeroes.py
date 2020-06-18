ffd0ac5807b6485bbb73b5003c7a33e024807008
Erik Rodriguez <eric19.21.03@gmail.com>
2020-06-17 17:59:21 -0700

    Adds moving zeroes and single number functions

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,44 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    # set i = 0
+    i = 0
+    too_long = 0
+    zero_counter = 0
+    # set move_zero = True
+    move_zero = True
+    # make while loop run until move_zero is false (i)
+    while move_zero:
+        print(arr)
+        # safety net infinite loop
+        # if i > 500, then break
+        too_long += 1
 
-    pass
+        if too_long > 15:
+            break
+
+        # if i == length of array
+        if i == len(arr):
+            zeroes = [0] * zero_counter
+            arr.extend(zeroes)
+            print(arr)
+            return arr
+
+        # if arr[i] == 0
+        if arr[i] == 0:
+            # zero_counter += 1
+            zero_counter += 1
+            # arr.pop(i)
+            arr.pop(i)
+            # subtract 1 from i
+            i -= 1
+
+        # add 1 to i
+        i += 1
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,13 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
-
-    pass
+    for i in range(len(arr)):
+        if arr.count(arr[i]) == 1:
+            return arr[i]
 
 
 if __name__ == '__main__':
