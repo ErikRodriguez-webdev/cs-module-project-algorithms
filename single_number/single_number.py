d6b590d221375bf3cbf2eed2a1c8eb41b828f09e
Erik Rodriguez <eric19.21.03@gmail.com>
2020-07-22 14:24:04 -0700

    Adds comments to single number code

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -6,7 +6,10 @@ Returns: an integer
 
 def single_number(arr):
     # Your code here
+    # for loop through list
     for i in range(len(arr)):
+        # use method to count the number of times a value pops up in list
+        # if it equals one then return the value that is not a duplicate
         if arr.count(arr[i]) == 1:
             return arr[i]
 
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,35 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+
 def sliding_window_max(nums, k):
     # Your code here
+    # set answer_list here
+    answer_list = []
+    # set temp_list
+    temp_list = []
 
-    pass
+    # make for loop (i)
+    for i in range(len(nums)):
+        # append nums[i] to temp_list
+        temp_list.append(nums[i])
+
+        # check if len(temp_list) == k
+        if len(temp_list) == k:
+            # then find max value of temp_list and append to answer_list
+            value = max(temp_list)
+            answer_list.append(value)
+            # pop 0 index
+            temp_list.pop(0)
+
+    return answer_list
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
